9ec805281cdfbe780fff7743976b83e020ce3cfb
main <main@mains-MacBook-Pro.local>
2017-05-06 00:33:28 -0600

    iplemted partials with local data for portfolio form and nav bars

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -27,6 +27,7 @@ class PortfoliosController < ApplicationController
   end
   def edit
   @portfolio_item = Portfolio.find(params[:id])
+  
   end  
   def update
     @portfolio_item = Portfolio.find(params[:id])
