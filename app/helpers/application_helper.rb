6adb657f7c85c2870f7c39bd57f0c0e8d43d22f6
main <main@mains-MacBook-Pro.local>
2017-08-03 14:21:31 -0600

    implemted refactor for navigation and active status

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,48 @@ module ApplicationHelper
   def copyright_generator
     PillingViewTool::Renderer.copyright 'William Pilling', 'All rights reserved'
   end 
+  def nav_items
+    [
+      {
+        url: root_path ,
+        title: 'Home'
+      },
+      {
+        url: about_me_path,
+        title: 'About Me'
+
+      },
+      {
+        url: contact_path,
+        title: 'Contact'
+
+      },
+      {
+        url: blogs_path,
+        title: 'Blog'
+
+      },
+      {
+        url: portfolios_path,
+        title: 'Portfolio'
+
+      },
+
+    ]
+  end
+  def nav_helper stlye, tag_type
+    nav_links= ''
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{stlye} #{active? item[:url]}'> #{item[:title]} </a></#{tag_type}>"
+    end
+
+
+
+
+nav_links.html_safe
+  end
+
+  def active? path
+    "active" if current_page? path
+  end
 end
