2d2596b17b672a4f6e28a54a63621340695188ae
main <main@mains-MacBook-Pro.local>
2017-08-24 12:27:02 -0600

    itregrated cocoon for portfolio items nested form commponets

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -20,7 +20,7 @@ class PortfoliosController < ApplicationController
 
   def new
     @portfolio_item = Portfolio.new
-    3.times { @portfolio_item.technologies.build }
+   
   end
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
@@ -71,7 +71,7 @@ class PortfoliosController < ApplicationController
                                       :main_image,
                                       :thumb_image,
                                       :body, 
-                                      technologies_attributes: [:name])
+                                      technologies_attributes: [:id, :name ,:_destroy])
   end
   def set_portfolio_item 
     @portfolio_item = Portfolio.find(params[:id])
