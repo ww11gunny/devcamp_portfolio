c9f2d065615e70b98d590892aa7623f7e8fe94b9
main <main@mains-MacBook-Pro.local>
2017-09-16 19:58:40 -0600

    added radio buttons for status field for blogs

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -94,7 +94,7 @@
     
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :Body, :topic_id)
+      params.require(:blog).permit(:title, :Body, :topic_id, :Status)
     end
     def set_sidebar_topics
       @side_bar_topics = Topic.with_blogs
