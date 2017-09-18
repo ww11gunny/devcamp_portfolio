8abefe2ffedf38f2c3bfbbb5828c386ebc89ea91
main <main@mains-MacBook-Pro.local>
2017-09-17 19:37:12 -0600

    Implemted partial for admin actions

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -21,4 +21,11 @@ module BlogsHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+
+  def blog_staus_color blog
+    'color: red;'if blog.draft?
+      
+    
+    
+  end 
 end
